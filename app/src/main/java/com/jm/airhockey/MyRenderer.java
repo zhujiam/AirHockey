3430f3546383cc6fb845dea9b80355b7d1fda2a2
Archer <archer@lednets.com>
2021-08-25 14:54:08 +0800

    Chapter 5

--- app/src/main/java/com/jm/airhockey/MyRenderer.java
+++ app/src/main/java/com/jm/airhockey/MyRenderer.java
@@ -2,6 +2,7 @@ package com.jm.airhockey;
 
 import android.content.Context;
 import android.opengl.GLSurfaceView;
+import android.opengl.Matrix;
 
 import com.jm.airhockey.utils.ShaderHelper;
 import com.jm.airhockey.utils.TextResourceReader;
@@ -30,6 +31,9 @@ public class MyRenderer implements GLSurfaceView.Renderer {
     private static final int STRIDE = (POSITION_COMPONENT_COUNT + COLOR_COMPONENT_COUNT) * BYTES_PER_FLOAT;
     private static final String A_COLOR = "a_Color";
     private static final String A_POSITION = "a_Position";
+    private static final String U_MATRIX = "u_Matrix";
+    private final float[] mProjectionMatrix = new float[16];
+    private int uMatrixLocation;
     private int aColorLocation;
     private int aPositionLocation;
     private FloatBuffer vertexData;
@@ -40,11 +44,11 @@ public class MyRenderer implements GLSurfaceView.Renderer {
         this.mContext = context;
         float[] tableVertices = {
                     0,     0,   1f,   1f,   1f,
-                -0.5f, -0.5f, 0.7f, 0.7f, 0.7f,
-                 0.5f, -0.5f, 0.7f, 0.7f, 0.7f,
-                 0.5f,  0.5f, 0.7f, 0.7f, 0.7f,
-                -0.5f,  0.5f, 0.7f, 0.7f, 0.7f,
-                -0.5f, -0.5f, 0.7f, 0.7f, 0.7f,
+                -0.5f, -0.8f, 0.7f, 0.7f, 0.7f,
+                 0.5f, -0.8f, 0.7f, 0.7f, 0.7f,
+                 0.5f,  0.8f, 0.7f, 0.7f, 0.7f,
+                -0.5f,  0.8f, 0.7f, 0.7f, 0.7f,
+                -0.5f, -0.8f, 0.7f, 0.7f, 0.7f,
 
                 -0.5f, 0f, 1f, 0f, 0f,
                  0.5f, 0f, 0f, 0f, 1f,
@@ -72,6 +76,7 @@ public class MyRenderer implements GLSurfaceView.Renderer {
         glUseProgram(mProgram);
         aColorLocation = glGetAttribLocation(mProgram, A_COLOR);
         aPositionLocation = glGetAttribLocation(mProgram, A_POSITION);
+        uMatrixLocation = glGetUniformLocation(mProgram, U_MATRIX);
         vertexData.position(0);
         glVertexAttribPointer(aPositionLocation, POSITION_COMPONENT_COUNT, GL_FLOAT, false, STRIDE, vertexData);
         glEnableVertexAttribArray(aPositionLocation); // 使能数据
@@ -83,11 +88,19 @@ public class MyRenderer implements GLSurfaceView.Renderer {
     @Override
     public void onSurfaceChanged(GL10 gl, int width, int height) {
        glViewport(0, 0, width, height);
+       if (width > height) {
+           float aspectRatio = (float)width / (float)height;
+           Matrix.orthoM(mProjectionMatrix, 0, -aspectRatio, aspectRatio, -1f, 1f, -1f, 1f);
+       } else {
+           float aspectRatio = (float)height / (float)width;
+           Matrix.orthoM(mProjectionMatrix, 0,-1f, 1f, -aspectRatio, aspectRatio,-5f, 5f);
+       }
     }
 
     @Override
     public void onDrawFrame(GL10 gl) {
         glClear(GL_COLOR_BUFFER_BIT);
+        glUniformMatrix4fv(uMatrixLocation, 1, false, mProjectionMatrix, 0);
         glDrawArrays(GL_TRIANGLE_FAN, 0, 6);
         glDrawArrays(GL_LINES, 6, 2);
         glDrawArrays(GL_POINTS, 8, 1);
